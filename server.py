fe8fb292a7250da7b3e7d655951ade0252fd6ec7
Maria Mendiburo <mariamendiburo@Marias-MacBook-Air.local>
2017-03-06 14:01:20 -0800

    Firs files for Flask app

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import json
+from pprint import pprint
+
+app = Flask(__name__)
+
+
+
+
+
+@app.route('/')
+#def hello():
+    #return "Hello World!"
+
+
+def index():
+    with open('pins_formatted.json') as data_file:    
+        #data = json.load(data_file)
+        data = json.parse(data_file)
+    return render_template("page_of_pins.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
